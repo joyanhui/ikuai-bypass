b7ef25dfec2244cfd244eb69e64fad5dc2ba91c9
Your Name <you@example.com>
2024-07-03 10:09:27 +0800

    修复清理指定标签功能

--- dev_shell.sh
+++ dev_shell.sh
@@ -10,10 +10,14 @@ go run *.go  -r 1 -m ispdomain -c  /home/yh/workspace/ikuai-bypass/config_exampl
 
 go run *.go  -r clean -c  /home/yh/workspace/ikuai-bypass/config_example.yml  -login http://10.1.1.1,admin,123
 
-git tag -a v2.0.1-alpha2 -m "- 修复不添加-m参数无法执行的bug
+git tag -a v2.0.1-alpha3 -m "- 修复不添加-m参数无法执行的bug
 - 增加删除旧规则的顺序的开关控制参数
 - 修复清理模式无法清理端口分流和ip分组的bug
+- 修复修ip分组和端口分流按照指定标签清理规则无法清理的bug
+
 此版本未经过测试，请谨慎使用"
+
+
 git tag -a v2.0.1-alpha1 -m "增加删除旧规则的顺序的开关控制参数，此版本未经过测试，请谨慎使用"
 git tag -a v2.0.0-beta2 -m "修复不添加-m参数无法执行的bug"
 git tag -a v2.0.0-beta1 -m "增加ip分组和端口分流模式，增加命令行覆盖ikuai登陆参数模式。其他更新请参考readme或commit"
@@ -23,7 +27,8 @@ go run *.go  -r 1 -m ispdomain -delOldRule before  -c  /home/yh/workspace/ikuai-
 
 git tag -d v2.0.0-beta2
 git tag -d v2.0.0-beta1
-git tag -d v2.0.1-alpha1
+
+
 
 git push origin --tags
 

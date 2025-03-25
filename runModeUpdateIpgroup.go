0e5e91cbb872f9816e393dcffd0fc8fcc98afef8
dscao <dscao.com@gmail.com>
2025-03-25 15:05:37 +0800

    完善delOldRule参数

--- runModeUpdateIpgroup.go
+++ runModeUpdateIpgroup.go
@@ -10,7 +10,6 @@ func updateIpgroup() {
 		log.Println("登录爱快失败：", err)
 		return
 	}
-	log.Println("Tips: 在添加之前会强制删除所有备注包含 IKUAI_BYPASS 字符的ip分组和端口分流，不受delOldRule参数影响,后续版本会尝试完善 2024-10-04 by joyanhui")
 
 	//添加之前会强制删除所有备注包含 IKUAI_BYPASS 字符的ip分组和端口分流，效率更高，但如果添加失败原来的分组数据会丢失。
 	if *delOldRule == "before" {

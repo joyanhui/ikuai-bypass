c55c717358299dc75df28ffec07c8c6140c20ec3
dscao <dscao.com@gmail.com>
2025-03-25 10:10:13 +0800

    Update confYaml.go

--- confYaml.go
+++ confYaml.go
@@ -40,6 +40,8 @@ var conf struct {
 		Nexthop   string `yaml:"nexthop"`
 		SrcAddr   string `yaml:"src-addr"`
 		IpGroup   string `yaml:"ip-group"`
+		Mode   string `yaml:"mode"`
+		IfaceBand   string `yaml:"ifaceband"`
 	} `yaml:"stream-ipport"`
 }
 

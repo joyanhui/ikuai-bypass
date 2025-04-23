c974657c471aca226ba0d2fc9624cb47afc3c2a3
joyanhui <joyanhui@qq.com>
2025-04-23 17:51:54 +0800

    Upgrade nixpkgs version to 24.11

--- flake.nix
+++ flake.nix
@@ -4,39 +4,39 @@
   description = "A Nix-flake-based golang development environment .";
 
   inputs = {
-    #nixpkgs.url = "github:NixOS/nixpkgs/23.11";
-    #nixpkgs.url = "https://mirrors.ustc.edu.cn/nix-channels/nixpkgs-unstable/nixexprs.tar.xz";
-    nixpkgs.url = "https://mirrors.ustc.edu.cn/nix-channels/nixos-24.05/nixexprs.tar.xz";
+    nixpkgs.url = "github:NixOS/nixpkgs/24.11";
   };
 
-  outputs = { self , nixpkgs ,... }: let
-    # system should match the system you are running on
-    system = "x86_64-linux";
-    # system = "x86_64-darwin";
-  in {
-    packages."${system}".dev = let
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
-      packages = with pkgs; [
-          go_1_23
-          gopls
-          gcc
-          upx
-          jetbrains.goland # allowUnfree
-          nushell
-      ];
-    in pkgs.runCommand "dev-shell" {
-      # Dependencies that should exist in the runtime environment
-      buildInputs = packages;
-      # Dependencies that should only exist in the build environment
-      nativeBuildInputs = [ pkgs.makeWrapper ];
-    } ''
-      mkdir -p $out/bin/
-      ln -s ${pkgs.nushell}/bin/nu $out/bin/dev-shell
-      wrapProgram $out/bin/dev-shell --set GOPROXY https://goproxy.cn,direct
-      wrapProgram $out/bin/dev-shell --prefix PATH : ${pkgs.lib.makeBinPath packages}
-    '';
-  };
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      packages."${system}".dev =
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+          packages = with pkgs; [
+            go_1_23
+            gopls
+            gcc
+            upx
+            nushell
+          ];
+        in
+        pkgs.runCommand "dev-shell"
+          {
+            buildInputs = packages;
+            nativeBuildInputs = [ pkgs.makeWrapper ];
+          }
+          ''
+            mkdir -p $out/bin/
+            ln -s ${pkgs.nushell}/bin/nu $out/bin/dev-shell
+            wrapProgram $out/bin/dev-shell --set GOPROXY https://goproxy.cn,direct
+            wrapProgram $out/bin/dev-shell --prefix PATH : ${pkgs.lib.makeBinPath packages}
+          '';
+    };
 }
